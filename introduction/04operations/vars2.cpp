3d0286b0d990bda2386f5cb151b98c660c990b15
Ubuntu <ubuntu@ip-172-31-10-78.us-west-2.compute.internal>
2016-11-29 21:43:33 +0000

    variable initialization

---
+++ introduction/04operations/vars2.cpp
@@ -0,0 +1,18 @@
+#include <iostream> 
+
+using namespace std;	
+
+int main() {
+
+
+	// TODO[ ] initialize two int type variables to 
+	//         permit this .cpp file to correctly compile. 
+
+	Area = (l*w)*0.5;
+
+	cout << "The Area of a right triangle of height: " << h <<
+		" and lenght: " << l << " is " << A << endl;
+	return 1;
+	// TODO[ ] compile link and execute.
+
+}
