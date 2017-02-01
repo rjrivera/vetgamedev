e4fbcb1a24b628d06db0fcf5e8ab9ac12296bef5
Rob Rivera <raptor.media.studios@gmail.com>
2017-02-01 01:24:24 +0000

    control lesson complete

--- introduction/05flow-control/control2.cpp
+++ introduction/05flow-control/control2.cpp
@@ -1,29 +1,43 @@
 #include <iostream> 
 #include <string>
+#include <cmath> //library providing the power function [ double pow(double base, double power) ]
 
 
 using namespace std;	
 
 int main() {
-	//the fibonacci sequance is defined as fib_n = fib_n-1 + fib_n-2; [1, 1, 2, 3, 5, etc.]
-	//it's a fun interview question. 
-	//TODO[ ] implement a simple naive solution using the below code snippet to print out the 3rd - 10th numbers [2, 3, 5, 8, ...]
-	int a = 1; //hint, view a as fib_n-2
-	int b = 1; //view b as fib_n-1
-	int c;     //view c as fib_n
-
-	for (int i = 3; i < 10; i++) {
-
-
-		cout << "the " << i << "fib number is " << c << " ";
+	int a, b;
+	bool init = true;
+	if(init) {
+		init = false; //toggle init to false;
+		cout << "running init routine"  << endl;
+		a = 10;
+		b = a;
 	}
-	cout << endl;
-	//TODO[ ] count down from ten
-	for (   ) {
-		cout << endl;
+	if(init) {
+		cout << "Rerunning init because first routine failed" << endl;
+		int a = 10;
+		int b = a;
 	}
+	else {
+		cout << "back-up init routine not initiated\n";
+	}
+	
+	/*
+		A factorial is defined as N! = N*(n-1)*(n-2)...*1
+		TODO[ ] implement factorial such that a is defined as a! using the for-loop below
+		remember, as is- a = b = 10  use the '*=' compound operator. 
+		*/
+	for(int i = b-1; i > 0; i--) {
+		
+	}
+	cout << b << " factorial is => " << a << endl;
 
+	//TODO[ ] implement a control scheme that prints out all numbers divisible by 2 in the range 0 - 20;
+	// hint - you learned about '<' and '>' however to express boolean equality, use '==' !!!! SUPER IMPORTANT !!!
+	// common novice mistake is to use '=' which is legal to the compiler, but leads to bugs. 
+	//hint: 'if(10%2 == 0)' yields true because 10 divided by 2 yields a remainder of 0...10 is divisable by two. 
+	
 
 	return 1;
 }
-
