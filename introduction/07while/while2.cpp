31db5c8b698f80db5e70079534b31d5c719267e9
Rob Rivera <raptor.media.studios@gmail.com>
2017-02-03 22:07:15 +0000

    pushing more work

---
+++ introduction/07while/while2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+/*function increments provided variables by 2 and one, respectively. 
+*/
+void update(int& rabbit, int& tortoise) {
+	if (rabbit == 9) rabbit = 1;
+	else if(rabbit == 10) rabbit = 2;
+	else rabbit += 2;
+
+	if (tortoise == 10) tortoise = 1;
+	else tortoise++;
+}
+
+int main() {
+	int a = 2;
+	int b = 1;
+	/*
+	TODO[ ] complete the while loop statement to stop updateing the values when they are equal. 
+	*/
+	while (/* code here */ ) {
+		update(a, b);
+		//the compiler reads the next two lines as one statement - 
+		//statement is spread to two lines to improve readability. 
+		cout << "a => " << a << endl
+			<< "b => " << b << endl;
+	}
+
+	//TODO [ ] count to 50 by 5 using a while-loop. 
+	int c = 1;
+	
+	
+
+	return 1;
+}
